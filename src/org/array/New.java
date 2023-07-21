7b68826746225fa3220e1354acdf05c5b5dcd66a
Prabu22027 <prabucivil220@gmail.com>
2023-07-21 16:41:27 +0530

    New Project

---
+++ src/org/array/New.java
@@ -0,0 +1,73 @@
+package org.array;
+
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class New {
+
+	public static void main(String[] args) {
+	
+		Scanner K = new Scanner(System.in);
+		
+		System.out.println("S.no 1");
+		int S1 = K.nextInt();
+		
+		System.out.println("S.no 2");
+		int S2 = K.nextInt();
+		
+		System.out.println("S.no 3");
+		int S3 = K.nextInt();
+		
+		System.out.println("S.no 4");
+		int S4 = K.nextInt();
+		
+		System.out.println("S.no 5");
+		int S5 = K.nextInt();
+		
+		System.out.println("S.no 6");
+		int S6 = K.nextInt();
+		
+		System.out.println("S.no 7");
+		int S7 = K.nextInt();
+		
+		System.out.println("S.no 8");
+		int S8 = K.nextInt();
+		
+		System.out.println("S.no 9");
+		int S9 = K.nextInt();
+		
+		System.out.println("S.no 10");
+		int S10 = K.nextInt();
+		
+		
+		int total = S1+S2+S3+S4+S5+S6+S7+S8+S9+S10;
+		System.out.println("Total T:"+total);
+		
+		float avg = total/10;
+		System.out.println("AVG:"+avg);
+		
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+	}
+}
